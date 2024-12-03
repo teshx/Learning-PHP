f534705faeb336939a3cdad33e2b6397a8d9e0af
teshager habtie <teshie00001@gmail.com>
2024-12-03 12:15:58 -0600

    this some about arrays

--- php-examples/index.php
+++ php-examples/index.php
@@ -20,3 +20,40 @@ $d = $a * $b;
 $e = ($a + $b) * ($c / $d);
 echo $e;
 echo '<br>';
+// String Operator (Concatenation)
+$firstName = "Abebe";
+$lastName = "Kebede";
+$fullName = $firstName . " " . $lastName;
+echo $fullName;
+echo '<br>';
+// Comparison operator 
+$a = 5;
+$b = 7;
+echo ($a <= $b);
+echo '<br>';
+// Logical operators 
+echo (1 == 1 || 2 == 2 || 3 == 7);
+echo '<br>';
+// Array 
+// Declaring empty array
+$exampleArray = array();
+echo '<br>';
+// Adding values to array 
+$exampleArray = array(3, "Abebe", 5, 8, 99, true, "Kebede");
+print "<pre>";
+print_r($exampleArray);
+
+// Accessing Array values 
+$exampleArray = array(3, "Abebe", 5, 8, 99, true, "Kebede");
+echo $exampleArray[1];
+
+// array_push() function 
+$exampleArray = array(3, "Abebe", 5, 8, 99, true, "Kebede");
+array_push($exampleArray, "Almaz", "Challa");
+print "<pre>";
+print_r($exampleArray);
+
+// Count function 
+$exampleArray = array(3, "Abebe", 5, 8, 99, true, "Kebede");
+echo count($exampleArray);
+

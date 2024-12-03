ddd7c0fdb6067b7f5845f231b2832be536e33aaf
teshager habtie <teshie00001@gmail.com>
2024-12-03 12:14:30 -0600

    this is Arithematic operation

--- php-examples/index.php
+++ php-examples/index.php
@@ -7,3 +7,16 @@ $a = "";
 $a = "Abebe";
 echo $a;
 echo '<br>';
+// Arithmetic operators
+$a;
+$b;
+$c;
+$d;
+$e;
+$a = 2;
+$b = 3;
+$c = $a + $b;
+$d = $a * $b;
+$e = ($a + $b) * ($c / $d);
+echo $e;
+echo '<br>';

0b6916d2b33fb4de24f71ae9b726d713f3a16c4f
teshager habtie <teshie00001@gmail.com>
2024-12-05 12:38:39 -0600

    this is signup resive data from forms

---
+++ OOP/LoginProject/includes/signup.inc.php
@@ -0,0 +1,35 @@
+<?php
+if (isset($_POST["submit"])) {
+
+    //graping the data 
+    $uid = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdrepeat = $_POST["pwdrepeat"];
+    $email = $_POST["email"];
+
+    // echo "<pre>";
+    // print_r($_POST);
+    // echo "</pre>";
+
+
+    // echo "<div>
+    // <P>$uid</p>
+    // <P>$pwd</p>
+    // <P>$pwdrepeat</p>
+    // <P>$email</p>
+    // </div>
+    
+    // "
+
+
+    //instanitiate sign up controler
+    include "../classes/DB/dbh.classes.php";
+    include "../classes/AUth/signup.classes.php";
+    include "../classes/AUth/signup-contr.classes.php";
+
+    $signup = new signupcontr($uid, $pwd, $pwdrepeat, $email);
+    //Runing error handelers and user signup
+    $signup->signUpuser();
+    //going to back to frontpaage
+    header("location:../index.php?error=none");
+}

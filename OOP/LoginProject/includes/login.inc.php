4c30b7c15d6673dbb9dcdec363e0df601845ab4e
teshager habtie <teshie00001@gmail.com>
2024-12-05 12:40:06 -0600

    this is login and logout include classes

---
+++ OOP/LoginProject/includes/login.inc.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_POST["submit"])) {
+
+    //graping the data 
+    $uid = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+
+
+    //instanitiate sign up controler
+    include "../classes/DB/dbh.classes.php";
+    include "../classes/AUth/login.classes.php";
+    include "../classes/AUth/login-contr.classes.php";
+
+    $logins = new logincontr($uid, $pwd);
+    //Runing error handelers and user signup
+    $logins->loginuser();
+    //going to back to frontpaage
+    header("location:../index.php?error=none");
+}
---
+++ OOP/LoginProject/includes/logout.inc.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+
+
+
+//gobackto front page
+header("location:../index.php?error=none");
